bd4788fc5efba4d8d6c94624f768ec7fe533d558
dikascode <dikaemanuel@gmail.com>
2023-11-21 19:13:57 +0100

    Sample Project commit

--- app/src/main/java/ng/com/netpos/app/MainActivity.kt
+++ app/src/main/java/ng/com/netpos/app/MainActivity.kt
@@ -1,17 +1,14 @@
 package ng.com.netpos.app
 
-import android.content.Intent
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.netpluspay.netposbarcodesdk.RESULT_CODE_TEXT
 import com.netpluspay.netpossdk.NetPosSdk
 import com.netpluspay.netpossdk.emv.CardReadResult
 import com.netpluspay.netpossdk.utils.DeviceConfig
 import io.reactivex.disposables.CompositeDisposable
-import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
     private var cardResult: CardReadResult? = null
@@ -23,19 +20,8 @@ class MainActivity : ComponentActivity() {
         btn = findViewById(R.id.test_card_reading)
         NetPosSdk.writeTpkKey(DeviceConfig.TPKIndex, "6943DD4434E0B3C0D808D0FE2A590CD9", this)
         btn.setOnClickListener {
-            showCardDialog(this, 20, 10, compositeDisposable) { result ->
+            showCardDialog(this, 1, 1, compositeDisposable) { result ->
                 findViewById<TextView>(R.id.scan_result).text = result.toString()
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        data?.let {
-            if (it.hasExtra(RESULT_CODE_TEXT)) {
-                val text = it.getStringExtra(RESULT_CODE_TEXT)
-                Timber.e("QR text: $text")
-                Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
             }
         }
     }
