5a32542949c0312f9a29eb5cbc9198d133478598
dikascode <dikaemanuel@gmail.com>
2023-11-23 09:59:29 +0100

    removed bar code

--- netposbarcodesdk/src/main/java/com/netpluspay/netposbarcodesdk/CONSTANTS.kt
+++
@@ -1,8 +0,0 @@
-package com.netpluspay.netposbarcodesdk
-
-internal const val CAMERA_PERMISSION_REQUEST_CODE = 100
-const val PERMISSION_REQUEST_CODE = 200
-const val LONG_150 = 150L
-const val RESULT_CODE_TEXT = "result_code_text"
-const val NETPOS_BARCODE_SCANNER_RESULT = "netpos_barcode_scanner_result"
-const val NETPOS_BARCODE_SCANNER_ERROR = "netpos_barcode_scanner_error"
--- netposbarcodesdk/src/main/java/com/netpluspay/netposbarcodesdk/domain/NetPosQRCodeFoundListener.kt
+++
@@ -1,6 +0,0 @@
-package com.netpluspay.netposbarcodesdk.domain
-
-internal interface NetPosQRCodeFoundListener {
-    fun onQrCodeFound(text: String)
-    fun onErrorFound(errorMessage: String)
-}
