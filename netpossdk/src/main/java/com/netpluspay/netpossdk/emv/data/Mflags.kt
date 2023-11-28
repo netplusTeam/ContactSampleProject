ad3cf87225c99a6ede07ea4f95ff18b6e9727c9a
dikascode <dikaemanuel@gmail.com>
2023-11-28 11:19:06 +0100

    Gradle Clean up

--- netpossdk/src/main/java/com/netpluspay/netpossdk/emv/data/Mflags.kt
+++
@@ -1,7 +0,0 @@
-package com.netpluspay.netpossdk.emv.data // ktlint-disable filename
-
-data class MFlags(
-    val pinCard: String,
-    val pinType: Int,
-    val pinOffTryCnt: Boolean
-)
--- netpossdk/src/main/java/com/netpluspay/netpossdk/errors/POSPrinterException.kt
+++
@@ -1,3 +0,0 @@
-package com.netpluspay.netpossdk.errors
-
-class POSPrinterException(code: Int, message: String) : POSException(code, message)
